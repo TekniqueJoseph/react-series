ce00af48c4a694f7b9c99b3c1f59f971ad9db810
TekniqueJoseph <joejacksun@gmail.com>
2023-04-01 18:53:54 -0400

    added props

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import { Component } from 'react';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
-import Hello from './components/Hello';
+// import Hello from './components/Hello';
 
 
 class App extends Component {
   render() {
   return (
     <div className="App">
-      <Greet name = 'Bruce'/>
-      <Greet name = 'Clark'/>
-      <Greet name = 'Diana'/>
-      {/* <Welcome />
-      <Hello /> */}
+      <Greet name = 'Bruce' heroName = 'Batman'>
+        <p>This is children props</p>
+      </Greet>
+
+      <Greet name = 'Clark' heroName = 'Superman'>
+        <button>Action</button>
+      </Greet>
+
+      <Greet name = 'Diana' heroName = 'Wonderwoman'/>
+
+      <Welcome name = 'Bruce' heroName = 'Batman'/>
+      <Welcome name = 'Clark' heroName = 'Superman'/>
+      <Welcome name = 'Diana' heroName = 'Wonderwoman'/>
+      {/* <Hello /> */}
     </div>
   );
 }
